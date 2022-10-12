84afbf5b33a1adb8ea7c594a607776fceed04793
Gonzalo <gonzalo0avalos@gmail.com>
2022-10-12 12:25:59 -0300

    DB

--- DB/portfolio.sql
+++ DB/portfolio.sql
@@ -1,3 +1,2 @@
-SELECT *
-FROM experiencia_laboral
-WHERE persona_id = 3
+INSERT INTO persona
+VALUES (1, 'Gonzalo', 'Avalos', 'Costa Rica 2390', '2001-07-29', '1128938088', 'gonzalo0avalos@gmail.com', 'Soy estudiante de Lic. en informática', 'www.foto.com/foto')
